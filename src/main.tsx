96f96a4712774caac14d1fb2292d5e4b57c06d27
takeruOtani <otanitakeru.sub@gmail.com>
2025-11-19 23:21:04 +0900

    github pageの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRouter from "./router.tsx";
 import { theme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <CssBaseline />
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>
 );
